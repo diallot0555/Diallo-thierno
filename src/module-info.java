6dde908d42937c59e8b270ec0d82e5f10b748fde
unknown <diallot0555@gmail.com>
2024-09-27 23:42:59 +0000

    mon premier commit

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module Diallo_projet {
+	exports Appl;
+	requires javafx.base;
+	requires transitive javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	//requires javafx.swt;
+	requires javafx.web;
+	requires java.sql;
+	requires java.sql.rowset;
+	requires java.xml;
+	requires java.xml.crypto;
+	
+}
